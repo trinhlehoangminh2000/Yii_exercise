db5bb494b46b99c6a8e06fb2d360b5ea2ac41203
Minh <trinhlehoangminh2000@gmail.com>
2019-12-26 15:40:12 +0700

    Update navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,7 +39,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'Article', 'url' => ['/site/article']],
+            ['label' => 'Article', 'url' => ['/article/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
